9a0260e5b71cda937d78ca94d5897127a65fc69e
Steve Albonico <salbonico@gmail.com>
2019-01-29 18:02:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ end
 
 def self.find(id, db)
 array = db.execute("select pokemon.name,pokemon.type from pokemon where pokemon.id = ?;",1)
-poke_out = Pokemon.new(id:id,name:array[0][0],type:array[0][1],db:db,hp: = 60)
+poke_out = Pokemon.new(id:id,name:array[0][0],type:array[0][1],db:db,hp: 60)
 if poke_out.name == "Pikachu"
   poke_out.hp = 59
   return poke_out
