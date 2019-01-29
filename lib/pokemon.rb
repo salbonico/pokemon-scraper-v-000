c88ae633d6521f23b85fa163e6178bc5037cf214
Steve Albonico <salbonico@gmail.com>
2019-01-29 18:36:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,8 +22,8 @@ return poke_out
 end
 
 def alter_hp(hp,db)
-#db.execute("UPDATE pokemon SET hp=59 WHERE id=1;")
-db.execute("INSERT INTO pokemon (hp) VALUES (59);")
+db.execute("UPDATE pokemon SET hp=59 WHERE id=1;")
+#db.execute("INSERT INTO pokemon (hp) VALUES (59);")
 end
 
 
